91bf3cc428abe086310123ceefe9baa4122f0ded
Abishek Aditya <abishekaditya@outlook.com>
2018-02-20 18:32:57 +0530

    complete the functionality of API 1

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,6 +1,43 @@
 const rp = require('request-promise');
 const constants = require('../constants');
 
+const joinBooksWithRatings = booksInput => new Promise((resolve, reject) => {
+  const books = booksInput.map(book => ({
+    bookId: book.id,
+    author: book.Author,
+    name: book.Name,
+  }));
+
+  const bookRatingPromiseArr = [];
+
+  books.forEach((book) => {
+    const ratingUrl = `${constants.api2}/${book.bookId}/`;
+    const bookRatingPromise = rp({
+      method: 'GET',
+      url: ratingUrl,
+    });
+    bookRatingPromiseArr.push(bookRatingPromise);
+  });
+
+  Promise.all(bookRatingPromiseArr)
+    .then((ratingObj) => {
+      for (let i = 0; i < books.length; i += 1) {
+        books[i].rating = JSON.parse(ratingObj[i]).rating;
+      }
+    })
+    .then(() => { resolve(books); })
+    .catch((reason) => { reject(new Error(reason.message)); });
+});
+
+const groupBooks = (books) => {
+  const group = {};
+  books.forEach((elem) => {
+    if (group[elem.author] === undefined) { group[elem.author] = [elem]; }
+    group[elem.author].push(elem);
+  });
+  return group;
+};
+
 module.exports = [
   {
     path: '/books',
@@ -9,7 +46,12 @@ module.exports = [
       rp({
         method: 'GET',
         url: constants.api1,
-      }).then(result => reply(result).code(200));
+      }).then(result => JSON.parse(result))
+        .then(booksObj => booksObj.books)
+        .then(booksArr => joinBooksWithRatings(booksArr))
+        .then(booksWithRatingsArr => groupBooks(booksWithRatingsArr))
+        .then(groupedBooksArr => reply(groupedBooksArr).code(200))
+        .catch(err => reply(err).code(501));
     },
   },
 ];
--- tests/routes/books.test.js
+++ tests/routes/books.test.js
@@ -1,13 +1,30 @@
 const server = require('../../src/server');
 
-describe('get details from /books', () => {
-  test('should return 200  statusCode for successful request', (done) => {
+describe('get details from /books should return', () => {
+  test('200 statusCode for successful request', (done) => {
     const request = {
       method: 'GET',
-      url: '/notes',
+      url: '/books',
     };
     server.inject(request, (reply) => {
       expect(reply.statusCode).toEqual(200);
+      done();
+    });
+  });
+
+  test('an object containing authors as keys and array of their books as value', (done) => {
+    expect.assertions(4);
+    const request = {
+      method: 'GET',
+      url: '/books',
+    };
+    server.inject(request, (reply) => {
+      const authorArr = reply.result[Object.keys(reply.result)[0]];
+      expect(authorArr[0]).toHaveProperty('rating');
+      expect(authorArr[0]).toHaveProperty('name');
+      expect(authorArr[0]).toHaveProperty('author');
+      expect(authorArr[0]).toHaveProperty('bookId');
+
       done();
     });
   });
